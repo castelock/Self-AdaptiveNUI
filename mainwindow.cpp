33212af5292cc5cd4d002ef0e353276509833353
castelock <catelock@gmail.com>
2017-09-19 11:33:27 +0200

    cout included

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,7 +1,9 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <opencv2/opencv.hpp>
+#include <iostream>
 
+using namespace std;
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -9,6 +11,7 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     cv::Mat inputImage = cv::imread("/home/acg/Pictures/acglogo.png");
     if(!inputImage.empty()) cv::imshow("Display Image", inputImage);
+cout<<"Goal achieved!!!!";
 
     ui->setupUi(this);
 }
